1eca92983899bc7ea5b9c4140777acb7c196be36
Neeraj_Sharma <neerajsharmanitc@gmail.com>
2022-10-06 23:15:28 +0530

    saving to github

--- src/ClassComponent/App.jsx
+++ src/ClassComponent/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import News from "./News";
+import Test from "./Test";
 
 export default class App extends Component {
   constructor() {
@@ -16,60 +17,65 @@ export default class App extends Component {
   };
   render() {
     return (
-      <BrowserRouter>
-        <Navbar changeLanguage={this.changeLanguage} />
-        <Routes>
-          <Route
-            path="/"
-            element={<News language={this.state.language} q="all" />}
-          />
-          <Route
-            path="/politics"
-            element={<News language={this.state.language} q="Politics" />}
-          />
-          <Route
-            path="/science"
-            element={<News language={this.state.language} q="Science" />}
-          />
-          <Route
-            path="/covid"
-            element={<News language={this.state.language} q="Covid" />}
-          />
-          <Route
-            path="/technology"
-            element={<News language={this.state.language} q="Technology" />}
-          />
-          <Route
-            path="/education"
-            element={<News language={this.state.language} q="Education" />}
-          />
-          <Route
-            path="/crime"
-            element={<News language={this.state.language} q="Crime" />}
-          />
-          <Route
-            path="/cricket"
-            element={<News language={this.state.language} q="Cricket" />}
-          />
-          <Route
-            path="/sport"
-            element={<News language={this.state.language} q="Sport" />}
-          />
-          <Route
-            path="/entertainment"
-            element={<News language={this.state.language} q="Entertainment" />}
-          />
-          <Route
-            path="/business"
-            element={<News language={this.state.language} q="Business" />}
-          />
-          <Route
-            path="/jokes"
-            element={<News language={this.state.language} q="Jokes" />}
-          />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <>
+        <BrowserRouter>
+          <Navbar changeLanguage={this.changeLanguage} />
+          <Routes>
+            <Route
+              path="/"
+              element={<News language={this.state.language} q="all" />}
+            />
+            <Route
+              path="/politics"
+              element={<News language={this.state.language} q="Politics" />}
+            />
+            <Route
+              path="/science"
+              element={<News language={this.state.language} q="Science" />}
+            />
+            <Route
+              path="/covid"
+              element={<News language={this.state.language} q="Covid" />}
+            />
+            <Route
+              path="/technology"
+              element={<News language={this.state.language} q="Technology" />}
+            />
+            <Route
+              path="/education"
+              element={<News language={this.state.language} q="Education" />}
+            />
+            <Route
+              path="/crime"
+              element={<News language={this.state.language} q="Crime" />}
+            />
+            <Route
+              path="/cricket"
+              element={<News language={this.state.language} q="Cricket" />}
+            />
+            <Route
+              path="/sport"
+              element={<News language={this.state.language} q="Sport" />}
+            />
+            <Route
+              path="/entertainment"
+              element={
+                <News language={this.state.language} q="Entertainment" />
+              }
+            />
+            <Route
+              path="/business"
+              element={<News language={this.state.language} q="Business" />}
+            />
+            <Route
+              path="/jokes"
+              element={<News language={this.state.language} q="Jokes" />}
+            />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+        <Test />
+      </>
     );
   }
 }
---
+++ src/ClassComponent/Test.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Test() {
+  return (
+    <div>
+      <h1>hello neeraj</h1>
+    </div>
+  );
+}
